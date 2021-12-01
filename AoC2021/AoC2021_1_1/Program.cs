9139aa1db37ebd2d11182c20783f607ddadd0f65
Johan de Geus <j.degeus@gmail.com>
2021-12-01 08:30:45 +0100

    Een goed begin.....

---
+++ AoC2021/AoC2021_1_1/Program.cs
@@ -0,0 +1,16 @@
+﻿int count = 0;
+
+int previous = int.MaxValue;
+
+while(true)
+{
+    var input = Console.ReadLine();
+    if (input == String.Empty) break;
+
+    var value = int.Parse(input);
+    if (value > previous) count++;
+    previous = value;
+}
+
+Console.WriteLine();
+Console.WriteLine(count);
---
+++ AoC2021/AoC2021_1_2/Program.cs
@@ -0,0 +1,23 @@
+﻿int count = 0;
+
+int previous = int.MaxValue;
+var values=new List<int>();
+
+while (true)
+{
+    var input = Console.ReadLine();
+    if (input == String.Empty) break;
+
+    values.Add(int.Parse(input));
+}
+
+var valuesArray= values.ToArray();
+
+for (int i = 2;i < valuesArray.Length;i++)
+{
+    var sum = valuesArray[i]+valuesArray[i-1]+valuesArray[i-2];
+    if (sum > previous) count++;
+    previous = sum;
+}
+Console.WriteLine();
+Console.WriteLine(count);
