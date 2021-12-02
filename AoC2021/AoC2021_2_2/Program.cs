80106c37a3de67bb77a41cc6da7ab81c0fd5cdb6
Johan de Geus <j.degeus@gmail.com>
2021-12-02 13:16:36 +0100

    Update Program.cs

--- AoC2021/AoC2021_2_2/Program.cs
+++ AoC2021/AoC2021_2_2/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var depth = 0;
+var position = 0;
+var aim = 0;
+
+while (true)
+{
+    var input = Console.ReadLine();
+    if (input == String.Empty) break;
+
+    var parts = input.Split(' ');
+    switch (parts[0])
+    {
+        case "forward":
+            var x = int.Parse(parts[1]);
+            position += x;
+            depth += aim * x;
+            break;
+        case "down":
+            aim += int.Parse(parts[1]);
+            break;
+        case "up":
+            aim -= int.Parse(parts[1]);
+            break;
+    }
+}
+Console.WriteLine();
+Console.WriteLine(depth * position);
+
