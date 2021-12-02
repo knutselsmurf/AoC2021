7d6b2882c86ff9ff47832270f2e741aebaca63f9
Johan de Geus <j.degeus@gmail.com>
2021-12-02 08:32:56 +0100

    Dag 2

---
+++ AoC2021/aoC2021_2_1/Program.cs
@@ -0,0 +1,25 @@
+﻿var depth = 0;
+var position = 0;
+
+while (true)
+{
+    var input = Console.ReadLine();
+    if (input == String.Empty) break;
+
+    var parts = input.Split(' ');
+    switch (parts[0])
+    {
+        case "forward":
+            position+= int.Parse(parts[1]);
+            break;
+        case "down":
+            depth +=int.Parse(parts[1]);
+            break;
+        case "up":
+            depth -=int.Parse(parts[1]);
+            break;  
+    }
+}
+Console.WriteLine();
+Console.WriteLine(depth * position);
+
