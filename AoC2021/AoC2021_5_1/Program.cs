66d268227026ca87b8b71e0538d6c5c24c51d6e4
Johan de Geus <j.degeus@gmail.com>
2021-12-05 19:16:21 +0100

    5 op een rij

---
+++ AoC2021/AoC2021_5_1/Program.cs
@@ -0,0 +1,36 @@
+﻿var grid = new int[1000, 1000];
+
+while (true)
+{
+    var input = Console.ReadLine();
+    if (input == String.Empty) break;
+
+    var p = input.Split(' ');
+    var p1 = p[0].Split(',').Select(_ => int.Parse(_)).ToArray();
+    var p2 = p[2].Split(',').Select(_ => int.Parse(_)).ToArray();
+
+    if (p1[0] == p2[0])
+    {
+        for(int i = Math.Min(p1[1],p2[1]);i<=Math.Max(p1[1],p2[1]);i++)
+        {
+            grid[p1[0], i]++;
+        }
+    }
+
+    if (p1[1] == p2[1])
+    {
+        for (int i = Math.Min(p1[0], p2[0]); i <= Math.Max(p1[0], p2[0]); i++)
+        {
+            grid[i,p1[1]]++;
+        }
+    }
+}
+
+var count = 0;
+foreach (var cell  in grid)
+{
+    if (cell > 1) count++;
+}
+
+Console.WriteLine();
+Console.WriteLine(count);   
---
+++ AoC2021/AoC2021_5_2/Program.cs
@@ -0,0 +1,90 @@
+﻿var grid = new int[1000, 1000];
+
+while (true)
+{
+    var input = Console.ReadLine();
+    if (input == String.Empty) break;
+
+    var p = input.Split(' ');
+    var p1 = p[0].Split(',').Select(_ => int.Parse(_)).ToArray();
+    var p2 = p[2].Split(',').Select(_ => int.Parse(_)).ToArray();
+
+    if (p1[0] == p2[0])
+    {
+        for (int i = Math.Min(p1[1], p2[1]); i <= Math.Max(p1[1], p2[1]); i++)
+        {
+            grid[p1[0], i]++;
+        }
+    }
+
+    else if (p1[1] == p2[1])
+    {
+        for (int i = Math.Min(p1[0], p2[0]); i <= Math.Max(p1[0], p2[0]); i++)
+        {
+            grid[i, p1[1]]++;
+        }
+    }
+    else
+    {
+        if (p1[0] < p2[0])
+        {
+            if (p1[1] < p2[1])
+            {
+                for (var i = p1[0]; i <= p2[0]; i++)
+                {
+                    grid[i, i - p1[0] + p1[1]]++;
+                }
+            }
+            else
+            {
+                for (var i = p1[0]; i <= p2[0]; i++)
+                {
+                    grid[i, -i + p1[0] + p1[1]]++;
+                }
+
+            }
+        }
+        else
+        {
+            if (p1[1] < p2[1])
+            {
+                for (var i = p2[0]; i <= p1[0]; i++)
+                {
+                    grid[i, -1*(i - p2[0]) + p2[1]]++;
+                }
+            }
+            else
+            {
+                for (var i = p2[0]; i <= p1[0]; i++)
+                {
+                    grid[i, i - p2[0] + p2[1]]++;
+                }
+
+            }
+
+        }
+    }
+
+}
+
+Console.WriteLine();
+for(var i=0;i<10;i++)
+{
+    for (var j=0;j<10;j++)
+    {
+        Console.Write(grid[j,i]);
+
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+
+var count = 0;
+foreach (var cell in grid)
+{
+    if (cell > 1) count++;
+}
+
+Console.WriteLine();
+Console.WriteLine(count);
